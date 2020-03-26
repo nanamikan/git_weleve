376421b07880581620201b93bba0c0b96a7d95cf
EC2 Default User <ec2-user@ip-172-31-43-42.ap-northeast-1.compute.internal>
2020-03-26 07:22:31 +0000

    update for deploy

--- config/application.rb
+++ config/application.rb
@@ -9,10 +9,12 @@ Bundler.require(*Rails.groups)
 module Weleve
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
-    config.web_console.whitelisted_ips = '61.89.88.135'
     config.i18n.default_locale = :ja
-
+    unless Rails.env.production?
+      # config.load_defaults 5.2
+      # config.web_console.whitelisted_ips = '61.89.88.135'
+      
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
