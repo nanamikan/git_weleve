619a642ed4df2e0f345284dbd1a70839e85470ff
nanamikan <nana.mikan_love.0627@i.softbank.jp>
2020-03-27 05:05:31 +0000

    sign_up_path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,15 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_in, keys: [:grade])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name, :preference,:grade,:intro,:image])
     # devise_parameter_sanitizer.permit(追加したいメソッドの種類, keys: [:パラメーター1, :パラメーター2,..])
-
+  end
+  
+  
+  def after_sign_in_path_for(resource)
+    root_path # ログイン後に遷移するpathを設定
+  end
+  
+  def after_sign_up_path_for(resource)
+    root_path # ログイン後に遷移するpathを設定
   end
   
  
