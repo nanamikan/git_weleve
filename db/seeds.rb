5a85267c6fee834e3ed63820ebee896a1bc81bc9
nanamikan <nana.mikan_love.0627@i.softbank.jp>
2020-03-27 07:49:50 +0000

    inserted data into connections table

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,3 @@
 Student.create(email: 'nnnnnnn.0627@gmail.com', password: 'Happylife0627', grade: 3)
 Group.create(id: 1, name: 'サークル1')
-# Connection.create(student_id: 1, group_id: 1)
+Connection.create(student_id: 1, group_id: 1)
