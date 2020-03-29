7bc821e3814a1c1bea44538f488e2b9b1d5a777d
nanamikan <nana.mikan_love.0627@i.softbank.jp>
2020-03-29 12:40:33 +0000

    add connction data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
-# Student.create(email: 'nnnnnnn.0627@gmail.com', password: 'Happylife0627', grade: 3)
 # Group.create(id: 1, name: 'サークル1')
 # Connection.create(student_id: 1, group_id: 1)
 # Group.create(id: 2, name: 'パリピ')
-Group.create(id: 3, name: 'みかん同好会')
-Group.create(id: 4, name: '水泳体育会')
-# Connection.create(student_id: 11, group_id:  2)
+# Group.create(id: 3, name: 'みかん同好会')
+# Group.create(id: 4, name: '水泳体育会')
+Connection.create(student_id: 11, group_id:  2)
+Connection.create(student_id: 21, group_id:  3)
 
 
