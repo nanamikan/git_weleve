8ec5d5c732644d19819acfce682302b5c40cff01
nanamikan <nana.mikan_love.0627@i.softbank.jp>
2020-03-29 12:59:24 +0000

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,10 @@
 # Group.create(id: 2, name: 'パリピ')
 # Group.create(id: 3, name: 'みかん同好会')
 # Group.create(id: 4, name: '水泳体育会')
-Connection.create(student_id: 11, group_id:  2)
-Connection.create(student_id: 21, group_id:  3)
+# Connection.create(student_id: 11, group_id:  2)
+# Connection.create(student_id: 21, group_id:  3)
+
+ Group.create(id: 5, name: 'バジリッサ')
+ Connection.create(student_id: 31, group_id:  5)
 
 
