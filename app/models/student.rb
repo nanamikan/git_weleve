58659c8e8e0da7c90bef1ef2d46721b9a3d11037
nanamikan <nana.mikan_love.0627@i.softbank.jp>
2020-05-25 11:04:55 +0000

    modified student validation

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,6 +1,5 @@
 class Student < ApplicationRecord
-# ユーザーのレコードと画像を紐づけることができます
-# ユーザーテーブルにカラムを追加する必要はありません。
+# ユーザーのレコードと画像を紐づけ
   has_one_attached :avatar
 
   # Include default devise modules. Others available are:
@@ -8,28 +7,19 @@ class Student < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
          
-        # アソシエーション
   has_many :applies, dependent: :destroy
   has_many :events, through: :applies
   
   has_many :connections, dependent: :destroy
   has_many :groups, through: :connections
+  # 他のモデルを一括で更新、保存
   accepts_nested_attributes_for :connections
-#   accepts_nested_attributes_for というkeyは、
-# 他のモデルを一括で更新、保存できるようにするもの
-
- # バリデーション
+ 
   validates :password, confirmation: true,on: :create
   validates :password_confirmation, presence: true,on: :create
-  
-  # 存在しなかったらエラーになる
   validates_presence_of  :password,:grade,:email,on: :create
-  validates :intro ,presence: { message: 'は60文字までです。' }
-  # :preference,:avatar,on: :update
-  # introは40字まで
   validates :intro,    length: { maximum: 60 }   
   
-  # rubyでは真偽値を返すメソッドには最後に?を付ける
   def full_profile?
     name?&&preference?&&intro?&&avatar.attached?  
   end
